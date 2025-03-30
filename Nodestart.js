a75231910990502f6a0cb4db3f4d3253c80c1cbe
Navin-songara <navinsongara1@gmail.com>
2025-03-30 13:59:31 +0530

    Add files via upload
    ws

---
+++ Nodestart.js
@@ -0,0 +1,22 @@
+// node js :-advance java script library to execute java script without browser. 
+// node js provides runtime enviernment to execute java script out of browser. 
+// node JS provides CLI cli stands for command line interface.with the help of CLI we can execute js prgram without browser. 
+// npm :-node package manager:-npm is a tool which is used to manage libraries and packages for nodejs. 
+// with the help of npm we can install libraries. 
+// npm provides the setup for node execution.
+
+// Read User Input 
+const readline = require('readline').createInterface({ 
+    input: process.stdin, 
+    output: process.stdout, 
+    }); 
+    var a,b,c; 
+    readline.question("Enter value of a", num => { 
+    a=parseInt(num); 
+    readline.question("Enter value of b", num => { 
+    b=parseInt (num); 
+    readline.close(); 
+    c=a+b; 
+    console.log("sum of a and b="+c); 
+    }); 
+}); 
