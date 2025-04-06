185eff6c5c9035e91c0896b1c548d52ffa6da01b
Navin-songara <navinsongara1@gmail.com>
2025-04-06 16:08:36 +0530

    Add files via upload
    aaaaaaaa

---
+++ consmultiinheri.js
@@ -0,0 +1,14 @@
+// Example default constructor with multilevel inheritance
+
+class A
+{
+    constructor()
+    {
+        this.x=10;
+        console.log("default class A");
+    }
+    showX()
+    {
+        console.log("value ")
+    }
+}
