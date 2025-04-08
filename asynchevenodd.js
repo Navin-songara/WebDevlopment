2e0653508cbed27a980b9e85d8d49a176fcca345
Navin-songara <navinsongara1@gmail.com>
2025-04-08 23:25:49 +0530

    Add files via upload
    check even or odd asychronize

---
+++ asynchevenodd.js
@@ -0,0 +1,27 @@
+// Example check even or odd using non blocking or asynchronize.
+
+const check = (num) => {
+    return new Promise((resolve, reject) => {
+        let res;
+        if (typeof num === 'number') {
+            if (num % 2 === 0) {
+                res = "Number is Even";
+            } else {
+                res = "Number is Odd";
+            }
+            resolve(res);
+        } else {
+            reject('The number provided must be of type number');
+        }
+    });
+};
+
+// Calling the async function
+(async () => {
+    try {
+        const result = await check('A');  // This will reject because 'A' is not a number
+        console.log('Result = ' + result);
+    } catch (err) {
+        console.log('Error = ' + err);
+    }
+})();
