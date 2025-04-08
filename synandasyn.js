69c8ae4123eef60f30921bc4eb3df2599e91b341
Navin-songara <navinsongara1@gmail.com>
2025-04-08 23:20:41 +0530

    Add files via upload
    aa

---
+++ synandasyn.js
@@ -0,0 +1,14 @@
+// Synchronous and asynchronous
+            OR 
+// Blocking And non-blocking method
+
+// Blocking Method Execute Synchronously. step by step execution means that the program 
+// is executed line. The program waits until the called function or the operation returns.
+
+// Non-blocking methods execute asynchronously. it refers to the program that does not 
+// block the execution of further operation . Non-blocking methods are execute 
+// asychronously means that the program may not necessarily execute line by line .
+// The program calls the function and move to the next operation does not wait for
+// it ot return.
+
+// Java Script library provides named class for implanention of Asynchronize programming
