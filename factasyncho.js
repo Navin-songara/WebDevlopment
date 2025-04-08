e03826ff48562965458dc9da77b4eedf51425d9a
Navin-songara <navinsongara1@gmail.com>
2025-04-08 23:24:29 +0530

    Add files via upload
    factorial using asychronize

---
+++ factasyncho.js
@@ -0,0 +1,23 @@
+const factorial = (num) => {
+    return new Promise((resolve, reject) => {
+        if (typeof num === 'number') {
+            let f = 1;
+            for (let i = 1; i <= num; i++) {
+                f = f * i;
+            }
+            resolve(f);
+        } else {
+            reject('The number provided must be of type number');
+        }
+    });
+};
+
+// Calling the asynchronous function
+(async () => {
+    try {
+        const result = await factorial(6);
+        console.log('Factorial =', result);
+    } catch (err) {
+        console.log('Error:', err);
+    }
+})();
