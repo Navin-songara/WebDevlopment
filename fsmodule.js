e9ea013ac7ef0d046ed420f52f7facd434d9b148
Navin-songara <navinsongara1@gmail.com>
2025-04-10 20:42:51 +0530

    Add files via upload
    fs module

---
+++ fsmodule.js
@@ -0,0 +1,24 @@
+// fs module:- file system module, it is used to manage file operations .
+// fs module provides functions to store the data in file and read data from a file
+// fs is inbuilt module.
+
+// functions:- 
+// 1.writeFile():- used to write data
+// 2.readFile():-used to read data in file.
+
+// Example read user input and write file
+
+var fs = require("fs");
+const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
+
+readline.question("Enter some text: ", (text) => {
+    var str = text;
+    fs.writeFile("DataSaved.txt", str, (err) => {
+        if (err) {
+            console.log("Error writing to file", err);
+        } else {
+            console.log("Data saved to file!");
+        }
+    });
+    readline.close();
+});
