29325a1282c10dc044e2053f32bbb85bcf145253
Navin Songara <navinsongara1@gmail.com>
2025-04-16 23:18:05 +0530

    Add files via upload
    okok

---
+++ Express.js
@@ -0,0 +1,60 @@
+// Advance node js for backend development. it provides advance library for web application
+//  and web API development .
+
+// Express is a rebust and flexible Node.js web application framework that provides a
+//  rebust set of features for web and mobile applications.
+
+// With the help of express js we can create web API.
+
+// API stands for Application programming Interface .
+//  API just deal with data. it provides portable and compact data like JSON,ATOM,XML etc.
+
+// Express js is used to define business logics / operations of web application.
+
+// With the help of express JS we can develop web API.
+
+// API :- Application programming interface.
+
+// API just deal with data and operations.
+
+// Follow CRUD operation.
+
+// C-Create or Save .
+// R-Read or Search or Show .
+// U-Update or modify .
+// D-Delete or remove .
+
+// Basic Methods of Standard API.
+
+// 1. Get - Send data from server to client.
+// 2. Post - Send data by client to server and save.
+// 3. Put - Send data by client to server and update.
+// 4. Delete - Send data by client to server and revome 
+
+// npm :- node package manages it provides setup to run time evirment and libraries to
+//  execute node js and express js applications. . .
+
+// >npm init -y
+
+// This Command is used to install express js libraries for express js application .
+// This libraries provides predefined  classes,functions and express classes,function 
+// and express programs execution envirment .
+
+// installation steps:-
+
+// 1. Global installation :- macine level installation :- one time
+//  >npm install express -g --save
+
+// 2. Application level installation each new application use bellow command :-
+
+// 1. For package.json 
+// >npm init --y
+
+// 2. load libray or node module 
+// >npm install express --save
+
+// 3. nodemon installation :- nodemon automate the execution process if we do some changes 
+// in code and save nodemon will automactically reflact the output with out recompile or 
+// restarting the application
+
+// >npm install nodemon -- save --dev
