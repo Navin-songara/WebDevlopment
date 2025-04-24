b9ed1b3eb354883040c6f70754aa48960039b800
Navin Songara <navinsongara1@gmail.com>
2025-04-24 17:30:20 +0530

    Add files via upload
    w

---
+++ webapi7.js
@@ -0,0 +1,16 @@
+// Basic Methods of WEB API
+
+// API :- Application programming interfere
+
+// API just deal with data.
+// We can use express api in react.java ,python,php.API is portable application
+// We can connect it with different types of application
+
+// METHODS
+
+// 1. GET:- Used to send data from server to client 
+// 2. POST:- Used to recieved data from server to client 
+// 3. PUT:- Used to recieved data from client and update in server
+// 4. DELETE:- Used to delete data in server
+
+// Make file with commands then download postman form chrome
